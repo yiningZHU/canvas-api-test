757d1276a595732d78b4117ce6731f9d5bd1e3b1
ZYN <1846364433@qq.com>
2017-03-05 20:14:27 -0800

    更改的第二周 作业完善

--- out/main.js
+++ out/main.js
@@ -10,33 +10,48 @@ window.onload = function () {
     image.src = "chicken.jpeg";
     image.onload = function () {
         var stage = new DisplayObjectContainer();
-        stage.x = 500;
-        stage.y = 500;
-        setInterval(function () {
+        var enterFrame = function () {
             context2D.clearRect(0, 0, canvas.width, canvas.height);
+            context2D.save();
             stage.draw(context2D);
-        }, 1000);
-        stage.alpha = 0.5;
+            context2D.restore();
+            window.requestAnimationFrame(enterFrame);
+        };
+        //每一帧进行刷新：告诉显卡：“我准备好了，可以开始画了～～～”
+        window.requestAnimationFrame(enterFrame);
         var text = new TextField();
         text.text = "Headache!!!!!";
         text.x = 100;
         text.y = 90;
-        text.alpha = 1;
-        var shrimp = new Bitmap();
-        shrimp.imgage = image;
-        shrimp.x = 100;
-        shrimp.y = 100;
-        shrimp.alpha = 0.5;
+        text.alpha = 0.5;
+        //text.scaleX = 2;
+        var crunchingBrother = new Bitmap();
+        crunchingBrother.imgage = image;
+        crunchingBrother.x = 100;
+        crunchingBrother.y = 100;
+        crunchingBrother.alpha = 0.5;
+        //crunchingBrother.scaleX = 2;
+        //crunchingBrother.roatation = 20;
         stage.addchild(text);
-        stage.addchild(shrimp);
+        stage.addchild(crunchingBrother);
     };
 };
 var DisplayObject = (function () {
     function DisplayObject() {
+        //坐标
         this.x = 0;
         this.y = 0;
+        //旋转
+        this.roatation = 0;
+        //缩放
+        this.scaleX = 1;
+        this.scaleY = 1;
+        //透明度
         this.alpha = 1;
         this.globalAlpha = 1;
+        //坐标要转换成矩阵
+        this.localMatrix = new Matrix(); //相对矩阵
+        this.globalMatrix = new Matrix(); //绝对矩阵
     }
     DisplayObject.prototype.draw = function (context) {
         if (this.parent) {
@@ -48,13 +63,22 @@ var DisplayObject = (function () {
             this.globalAlpha = this.alpha;
         }
         context.globalAlpha = this.globalAlpha;
+        //将数字转换成矩阵
+        this.localMatrix.convertFromDisplayObjectProperties(this.x, this.y, this.scaleX, this.scaleY, this.roatation);
+        if (this.parent) {
+            //有父亲，那么globalMatrix就等于自己的localMatrix乘以父亲的globalMatrix
+            this.globalMatrix = matrixAppendMatrix(this.localMatrix, this.parent.globalMatrix);
+        }
+        else {
+            //舞台没有父亲，让自己的localMatrix等于gloabalMatrix
+            this.globalMatrix = this.localMatrix;
+        }
+        context.setTransform(this.globalMatrix.a, this.globalMatrix.b, this.globalMatrix.c, this.globalMatrix.d, this.globalMatrix.tx, this.globalMatrix.ty);
         this.render(context);
     };
     DisplayObject.prototype.render = function (context) {
     };
-    DisplayObject.prototype.transform = function (x, y) {
-        this.tansMatrix[0][2] += x;
-        this.tansMatrix[1][2] += y;
+    DisplayObject.prototype.hitTest = function () {
     };
     return DisplayObject;
 }());
@@ -89,7 +113,7 @@ var TextField = (function (_super) {
     }
     TextField.prototype.render = function (context) {
         //context.globalAlpha = this.alpha;
-        context.fillText(this.text, this.x, this.y, 100);
+        context.fillText(this.text, 0, 0, 100);
     };
     return TextField;
 }(DisplayObject));
@@ -100,8 +124,43 @@ var Bitmap = (function (_super) {
     }
     Bitmap.prototype.render = function (context) {
         //context.globalAlpha = this.alpha;
-        context.drawImage(this.imgage, this.x, this.y);
+        context.drawImage(this.imgage, 0, 0);
     };
     return Bitmap;
 }(DisplayObject));
+//矩阵类
+var Matrix = (function () {
+    function Matrix() {
+        this.a = 1;
+        this.b = 0;
+        this.c = 0;
+        this.d = 1;
+        this.tx = 0;
+        this.ty = 0;
+    }
+    Matrix.prototype.convertFromDisplayObjectProperties = function (x, y, scaleX, scaleY, rotation) {
+        this.tx = x;
+        this.ty = y;
+        var skewX, skewY;
+        skewX = skewY = rotation / 180 * Math.PI;
+        var u = Math.cos(skewX);
+        var v = Math.sin(skewX);
+        this.a = Math.cos(skewY) * scaleX;
+        this.b = Math.sin(skewY) * scaleX;
+        this.c = -v * scaleY;
+        this.d = u * scaleY;
+    };
+    return Matrix;
+}());
+//矩阵相乘
+function matrixAppendMatrix(m1, m2) {
+    var result = new Matrix();
+    result.a = m1.a * m2.a + m1.b * m2.c;
+    result.b = m1.a * m2.b + m1.b * m2.d;
+    result.c = m2.a * m1.c + m2.c * m1.d;
+    result.d = m2.b * m1.c + m1.d * m2.d;
+    result.tx = m2.a * m1.tx + m2.c * m1.ty + m2.tx;
+    result.ty = m2.b * m1.tx + m2.d * m1.ty + m2.ty;
+    return result;
+}
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,111 +1,182 @@
 window.onload = () => {
     var canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
     var context2D = canvas.getContext("2d");
-   
-    var image = new Image ();
+
+    var image = new Image();
     image.src = "chicken.jpeg";
     image.onload = () => {
+        
         var stage = new DisplayObjectContainer();
-         stage.x = 500;
-         stage.y = 500;
-        setInterval(()=>{
-            context2D.clearRect(0,0,canvas.width,canvas.height);
+
+        let enterFrame = () => {
+            context2D.clearRect(0, 0, canvas.width, canvas.height);
+            context2D.save();
             stage.draw(context2D);
-        },1000);
-   stage.alpha = 0.5;
+            context2D.restore();
+            window.requestAnimationFrame(enterFrame);
+        }
 
-    var text = new TextField();
-    text.text = "Headache!!!!!";
-    text.x = 100;
-    text.y = 90;
-    text.alpha = 1;
+        //每一帧进行刷新：告诉显卡：“我准备好了，可以开始画了～～～”
+        window.requestAnimationFrame(enterFrame);
 
-    var shrimp = new Bitmap();
-    shrimp.imgage = image;
-    shrimp.x = 100;
-    shrimp.y = 100;
-    shrimp.alpha = 0.5;
+        var text = new TextField();
+        text.text = "Headache!!!!!";
+        text.x = 100;
+        text.y = 90;
+        text.alpha = 0.5;
+        //text.scaleX = 2;
 
-    stage.addchild(text);
-    stage.addchild(shrimp);
+        var crunchingBrother = new Bitmap();
+        crunchingBrother.imgage = image;
+        crunchingBrother.x = 100;
+        crunchingBrother.y = 100;
+        crunchingBrother.alpha = 0.5;
+        //crunchingBrother.scaleX = 2;
+        //crunchingBrother.roatation = 20;
+
+        stage.addchild(text);
+        stage.addchild(crunchingBrother);
     }
 };
 
-interface Drawable{
-    draw(context:CanvasRenderingContext2D);
+interface Drawable {
+    draw(context: CanvasRenderingContext2D);
 }
 
-class DisplayObject implements Drawable{
+class DisplayObject implements Drawable {
+
+    parent: DisplayObjectContainer;
+
+    //坐标
     x = 0;
     y = 0;
+    //旋转
+    roatation = 0;
+    //缩放
+    scaleX = 1;
+    scaleY = 1;
+    //透明度
     alpha = 1;
     globalAlpha = 1;
-    parent:DisplayObjectContainer;
-    tansMatrix : number[][];
 
-    draw(context:CanvasRenderingContext2D){
-        if(this.parent){
+    //坐标要转换成矩阵
+    localMatrix: Matrix = new Matrix();//相对矩阵
+    globalMatrix: Matrix = new Matrix();//绝对矩阵
+
+    draw(context: CanvasRenderingContext2D) {
+        if (this.parent) {
             //有父亲，那么globalAlpha就等于自己的alpha乘以父亲的globalAlpha
             this.globalAlpha = this.alpha * this.parent.globalAlpha;
         }
-        else{
+        else {
             //舞台没有父亲，让自己的alpha等于gloabalAlpha
             this.globalAlpha = this.alpha;
         }
-       
         context.globalAlpha = this.globalAlpha;
+
+        //将数字转换成矩阵
+        this.localMatrix.convertFromDisplayObjectProperties(this.x, this.y, this.scaleX, this.scaleY, this.roatation);
+        if (this.parent) {
+            //有父亲，那么globalMatrix就等于自己的localMatrix乘以父亲的globalMatrix
+            this.globalMatrix = matrixAppendMatrix(this.localMatrix, this.parent.globalMatrix);
+        }
+        else {
+            //舞台没有父亲，让自己的localMatrix等于gloabalMatrix
+            this.globalMatrix = this.localMatrix;
+        }
+        context.setTransform(this.globalMatrix.a,
+            this.globalMatrix.b,
+            this.globalMatrix.c,
+            this.globalMatrix.d,
+            this.globalMatrix.tx,
+            this.globalMatrix.ty);
         this.render(context);
     }
 
-    render(context:CanvasRenderingContext2D){
+    render(context: CanvasRenderingContext2D) {
 
     }
 
-    transform(x:number,y:number){
-        this.tansMatrix[0][2]+=x;
-        this.tansMatrix[1][2]+=y; 
-    }
+    hitTest() {
 
-    hitTest(){
-        
     }
 }
 
-class DisplayObjectContainer extends DisplayObject{
+class DisplayObjectContainer extends DisplayObject {
 
-    x : number = 0;
-    y : number = 0;
-    canvasarray : Drawable[] = [];
+    x: number = 0;
+    y: number = 0;
+    canvasarray: Drawable[] = [];
 
-    addchild(child:DisplayObject){
+    addchild(child: DisplayObject) {
         this.canvasarray.push(child);
         child.parent = this;
     }
 
-    render(context:CanvasRenderingContext2D){
-        for(let drawable of this.canvasarray){
+    render(context: CanvasRenderingContext2D) {
+        for (let drawable of this.canvasarray) {
             drawable.draw(context);
         }
     }
 }
 
-class TextField extends DisplayObject{
+class TextField extends DisplayObject {
     
-    text:string;
-    x:number = 0;
-    y:number = 0;
-    render(context:CanvasRenderingContext2D){
+    text: string;
+    x: number = 0;
+    y: number = 0;
+    render(context: CanvasRenderingContext2D) {
         //context.globalAlpha = this.alpha;
-        context.fillText(this.text,this.x,this.y,100);
+        context.fillText(this.text, 0, 0, 100);
     }
 
 }
 
-class Bitmap extends DisplayObject{
-    imgage:HTMLImageElement;
+class Bitmap extends DisplayObject {
+    imgage: HTMLImageElement;
 
-    render(context:CanvasRenderingContext2D){
+    render(context: CanvasRenderingContext2D) {
         //context.globalAlpha = this.alpha;
-        context.drawImage(this.imgage,this.x,this.y);
+        context.drawImage(this.imgage, 0, 0);
     }
+}
+
+//矩阵类
+class Matrix {
+
+    a = 1;
+    b = 0;
+    c = 0;
+    d = 1;
+    tx = 0;
+    ty = 0;
+
+    convertFromDisplayObjectProperties(x: number, y: number, scaleX: number, scaleY: number, rotation: number) {
+        this.tx = x;
+        this.ty = y;
+
+        var skewX, skewY;
+        skewX = skewY = rotation / 180 * Math.PI;
+        var u = Math.cos(skewX);
+        var v = Math.sin(skewX);
+        this.a = Math.cos(skewY) * scaleX;
+        this.b = Math.sin(skewY) * scaleX;
+        this.c = -v * scaleY;
+        this.d = u * scaleY;
+    }
+
+
+
+}
+
+//矩阵相乘
+function matrixAppendMatrix(m1: Matrix, m2: Matrix) {
+     var result = new Matrix();
+        result.a = m1.a * m2.a + m1.b * m2.c;
+        result.b = m1.a * m2.b + m1.b * m2.d;
+        result.c = m2.a * m1.c + m2.c * m1.d;
+        result.d = m2.b * m1.c + m1.d * m2.d;
+        result.tx = m2.a * m1.tx + m2.c * m1.ty + m2.tx;
+        result.ty = m2.b * m1.tx + m2.d * m1.ty + m2.ty;
+        return result;
 }
